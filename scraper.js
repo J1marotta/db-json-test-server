8aa6e2de1fb37d9231e2bb46360c4f4950d8f5eb
James Marotta <james.marotta@outlook.com>
2020-09-13 14:37:07 +1000

    return return return

--- scraper.js
+++ scraper.js
@@ -1,35 +1,49 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-const engine = 'Google'
-let page = 'Page01'
+async function main(mode) {
+  const pages = ['Page01', 'Page02', 'Page03', 'Page04', 'Page05']
 
-const url = `https://infotrack-tests.infotrack.com.au/${engine}/${page}.html`
+  const engine = {
+    google: 'Google',
+    bing: 'Bing'
+  }
 
-function main() {
-  axios
-    .get(url)
-    .then(res => {
+  const selectors = {
+    google: ['.r a', '.g'],
+    bing: ['.b_algo h2 a', '#b_results']
+  }
+
+  const results = []
+
+  const currentEngine = engine[mode]
+
+  await pages.map(async page => {
+    const url = `https://infotrack-tests.infotrack.com.au/${currentEngine}/${page}.html`
+    const currentSelector = selectors[mode]
+
+    try {
+      const res = await axios.get(url)
       const $ = cheerio.load(res.data)
-      const selectors = $('.r a', '.g').get()
-      const results = []
+      const selectors = $(currentSelector[0], currentSelector[1]).get()
 
       const href = 'https://www.infotrack.com.au'
 
       const targets = selectors.flatMap((x, i) =>
         $(x).attr('href').startsWith(href) ? [i] : []
       )
-
-      console.log({
-        targets
-      })
-    })
-    .catch(e => {
+      results.push({ [page]: targets })
+      console.log(results)
+    } catch (e) {
       console.error('FAILED', { status: e.status, msg: e.message })
-    })
+    }
+  })
+  return results
 }
 
-main()
+const fn = async () => await main('google')
+
+fn()
 
 // var href = 'https://www.infotrack.com.au'
 // ('.g .r a').flatMap((x,i) => x.href.startsWith(href) ? [i] : [])
