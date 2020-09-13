7a87d9a5879c7c34ca4666c96060c7dc020e418f
James Marotta <james.marotta@outlook.com>
2020-09-13 13:02:09 +1000

    cherio go

--- scraper.js
+++ scraper.js
@@ -1,40 +1,37 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 const engine = 'Google'
 let page = 'Page01'
 
 const url = `https://infotrack-tests.infotrack.com.au/${engine}/${page}.html`
 
-
 function main() {
-      
-    axios.get(url)
-      .then((res) => {
-
-
-    const $ = cheerio.load(res.data)
-      const all = $(".g")      
+  axios
+    .get(url)
+    .then(res => {
+      const $ = cheerio.load(res.data)
+      const selectors = $('.r a', '.g').get()
       const results = []
-      const r2 = new Set()
-      $("g").each((index, element) => {
-        r2.add($(element).text());
+
+      const href = 'https://www.infotrack.com.au'
+
+      const targets = selectors.flatMap((x, i) =>
+        $(x).attr('href').startsWith(href) ? [i] : []
+      )
+
+      console.log({
+        targets
       })
-
-
-        console.log(
-        results
-        ,r2    
-        )
-
     })
-    .catch( e => {
-      console.error('FAILED', {status: e.status, msg: e.message})
-
+    .catch(e => {
+      console.error('FAILED', { status: e.status, msg: e.message })
     })
+}
 
+main()
 
-  }
+// var href = 'https://www.infotrack.com.au'
+// ('.g .r a').flatMap((x,i) => x.href.startsWith(href) ? [i] : [])
 
-
-  main()
+//$$('#b_results .b_algo h2 a').flatMap( (x,i) =>  x.href.includes('infotrack') ? [i] : [])
